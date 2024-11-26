38bca7bb0d5ebcbd13d46e37c69aa394a90ef181
johnhelmuth <john@helmuth.org>
2024-11-25 17:00:48 -0800

    Added in some code to store clusters in LocalStorage.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './assets/main.css'
 import 'vue-final-modal/style.css'
 
-import { createApp } from 'vue'
+import { createApp, watch } from 'vue'
 import { createPinia } from 'pinia';
 
 import App from './App.vue'
@@ -16,5 +16,14 @@ const pinia = createPinia();
 app.use(vfm);
 app.use(pinia);
 app.use(router);
+
+// TODO : Figure out how to do this in ClustersStore.ts... possibly have to do a pinia plugin or a composable?
+watch(
+  pinia.state,
+  (state) => {
+    localStorage.setItem("pinia.cluster-map.clusters", JSON.stringify(state.clusters));
+  },
+  { deep: true }
+)
 
 app.mount('#app')
--- src/stores/ClustersStore.ts
+++ src/stores/ClustersStore.ts
@@ -1,22 +1,37 @@
-import {ref, reactive} from 'vue';
-import { defineStore } from 'pinia';
+import {defineStore} from 'pinia';
 
 import {ClustersModel} from "@/models/ClustersModel";
 import clusterJsonDLC from "@/data/clusters/cluster-DLC.json";
 import clusterJson1652 from "@/data/clusters/cluster-1652.json";
 import type {ClustersModelInterface} from "@/types/ClusterTypes";
+import {reactive} from "vue";
 
-const clustersData = [
-  clusterJsonDLC,
-  clusterJson1652
-];
+const defaultClustersData = {
+  currentClusterId: clusterJsonDLC.id,
+  clusters: [
+    clusterJsonDLC,
+    clusterJson1652
+  ]
+};
 
 export const useClustersStore = defineStore(
   "clusters",
-  {
-    state: () => {
-      const clusters: ClustersModelInterface = (new ClustersModel({currentClusterId: clusterJsonDLC.id, clusters: clustersData}));
-      return { clusters };
+  () => {
+    let clustersData;
+    const clustersDataFromLCJSON = localStorage.getItem("pinia.cluster-map.clusters");
+    if (clustersDataFromLCJSON) {
+      try {
+        clustersData = JSON.parse(clustersDataFromLCJSON).clusters;
+      } catch (e) {
+        console.error(e);
+        clustersData = undefined;
+      }
     }
+    clustersData = clustersData || defaultClustersData;
+    const clusters: ClustersModelInterface = reactive(new ClustersModel(clustersData));
+
+    return { clusters };
   }
-)
+);
+
+
