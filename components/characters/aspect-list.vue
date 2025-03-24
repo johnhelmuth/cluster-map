6c4425662ef32ebad4e8bcc704b9b05a8996fa73
John Helmuth <john@helmuth.org>
2025-03-24 09:58:33 -0700

    POC continued.  Refactored character sheet components to what might be several separately useful components. Added CharacterModel class to start adding CRUD operations to it.  First cut at stress box toggle on CharacterModel, tied to the stress track/box components' behavior.

---
+++ components/characters/aspect-list.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type {CharacterAspectData} from "~/types/character/CharacterTypes";
+
+defineProps<{
+  aspects?: Array<CharacterAspectData> | undefined
+}>();
+
+</script>
+
+<template>
+  <div v-if="aspects && aspects.length">
+    <h3>Aspects</h3>
+    <ul>
+      <li v-for="aspect in aspects" class="aspect-item property-item">
+        <div class="list-label aspect-type" v-if="aspect.aspectType">{{ aspect.aspectType }}:</div>
+        <div class="list-label aspect-type" v-else>-</div>
+        <div class="aspect-name value">{{ aspect.name }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/characters/stress-box.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  isChecked: boolean;
+  showSoakAmount?: boolean;
+  boxLimitPerAction?: number;
+  soakNumber?: number
+}>();
+
+const emit = defineEmits(["toggleBox"]);
+
+function toggleStressBox() {
+  console.log('StressBox.toggleStressBox() icon clicked.');
+  emit('toggleBox');
+}
+
+const shouldShowSoak = computed(() => {
+  return (typeof props?.showSoakAmount === 'boolean' ? props.showSoakAmount : true);
+});
+
+</script>
+
+<template>
+  <div class="stress-box-container" @click="toggleStressBox" :class="{ checked: isChecked}" :data[box-limit-per-action]="boxLimitPerAction">
+    <div v-if="shouldShowSoak" class="soak-value">{{ soakNumber }}</div>
+  </div>
+</template>
+
+<style scoped>
+
+.stress-box-container {
+  border: 2px solid var(--color-text);
+  border-radius: 3px;
+  font-size: 2.1rem;
+  width: 2rem;
+  height: 2rem;
+}
+.stress-box-container.checked {
+  background-color: var(--color-text);
+  color: var(--color-background)
+}
+
+.soak-value {
+  text-align: center;
+  font-size: 1.2rem;
+}
+</style>
