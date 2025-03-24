bf6e2b866bfc61f4e59b4a76a5f9602a1db459f3
John Helmuth <john@helmuth.org>
2025-03-24 09:58:33 -0700

    POC continued.  Refactored character sheet components to what might be several separately useful components. Added CharacterModel class to start adding CRUD operations to it.  First cut at stress box toggle on CharacterModel, tied to the stress track/box components' behavior.

---
+++ components/characters/refresh-list.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  "refresh": number,
+  "fatePoints": number
+}>();
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li class="property-item">
+        <div class="list-label refresh-label">Refresh</div>
+        <div class="refresh-value value">{{ refresh }}</div>
+      </li>
+      <li class="property-item">
+        <div class="list-label fate-points-label">Fate Points</div>
+        <div class="fate-points-value value">{{ fatePoints }}</div>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<style scoped>
+
+li.property-item {
+  align-items: center;
+}
+
+ul .list-label {
+  line-height: 1;
+}
+
+.value {
+  font-size: 2rem;
+}
+
+</style>
---
+++ components/characters/stunt-list.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+import type {StuntData} from "~/types/character/CharacterTypes";
+
+const props = defineProps<{
+  "stunts": StuntData[]
+}>();
+
+</script>
+
+<template>
+  <div>
+    <h3>Stunts</h3>
+    <ul>
+      <li v-for="stunt in stunts" class="stunt-item property-item">
+        <h4>{{ stunt.name }}</h4>
+        <p>{{ stunt.description }}</p>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<style scoped>
+
+.stunt-item > * {
+  grid-column: 1 / 3;
+}
+</style>
