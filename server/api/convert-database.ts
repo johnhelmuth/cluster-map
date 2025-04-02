150e1db844ca2bf75ce3c75ed550ef591f39bb0e
johnhelmuth <john@helmuth.org>
2025-04-01 20:02:03 -0700

    Minor tweaks and adding @zodyac/zod-mongoose library to create mongoose schema from zod schema.

--- server/api/convert-database.ts
+++ server/api/convert-database.ts
@@ -8,60 +8,61 @@ import {universesData} from '~/server/data/universesData'
 import {ClusterModel} from "~/models/ClusterModel";
 import {ClusterModelDataType} from "~/types/ClusterTypes";
 import {SystemModelDataType, SystemModelInterface} from "~/types/SystemTypes";
-// @ts-ignore
-import {mongoose} from "mongoose";
+import mongoose from "mongoose";
 import {StraitModelDataType, StraitModelInterface} from "~/types/StraitTypes";
 
 type IdsTo_IDsMapType = Map<string, typeof mongoose.Types.ObjectId>;
 
-function mapSystemIdsToObjectIds(systems: SystemModelDataType[]) : { idsToObjectIds: IdsTo_IDsMapType, systemDocuments: Array<SystemDocumentInterface>} {
+function mapSystemIdsToObjectIds(systems: SystemModelDataType[]): {
+  idsToObjectIds: IdsTo_IDsMapType,
+  systemDocuments: Array<SystemDocumentInterface>
+} {
   const systemDocuments: Array<SystemDocumentInterface> = [];
   const idsToObjectIds =
     systems.reduce((idsMap, system) => {
       const objectId = new mongoose.Types.ObjectId();
       if (system?.id) {
+        //@ts-ignore
         idsMap.set(system?.id, objectId);
         systemDocuments.push({...system, _id: objectId} as SystemDocumentInterface);
       }
       return idsMap;
     }, new Map<string, typeof mongoose.Types.ObjectId>());
-  return { idsToObjectIds, systemDocuments };
+  return {idsToObjectIds, systemDocuments};
 }
 
-async function clusterToModel(cluster: ClusterModelDataType) : Promise<[string, typeof ClusterDocument] | [string, Error] | undefined> {
+async function clusterToModel(cluster: ClusterModelDataType): Promise<[string, typeof ClusterDocument] | [string, Error] | undefined> {
   console.log('cluster.id: ', cluster.id);
-  if (typeof cluster.id === 'string') {
-    const systems = cluster.systems || [];
-    const straits = (cluster.straits || []) as StraitModelDataType[];
-    delete cluster.systems;
-    delete cluster.straits;
-    const clusterDocData = cluster as ClusterDocumentInterface;
-    if (systems?.length) {
-      const {idsToObjectIds, systemDocuments} = mapSystemIdsToObjectIds(systems);
+  const systems = cluster.systems || [];
+  const straits = (cluster.straits || []) as StraitModelDataType[];
+  delete cluster.systems;
+  delete cluster.straits;
+  const clusterDocData = cluster as ClusterDocumentInterface;
+  if (systems?.length) {
+    const {idsToObjectIds, systemDocuments} = mapSystemIdsToObjectIds(systems);
 
-      let straitsWith_ids: StraitDocumentInterface[] = [];
-      if (straits?.length) {
-        straitsWith_ids = straits.map((strait) => {
-          const systems = strait.systems.map((systemId) => {
-            return idsToObjectIds.get(systemId) as string | undefined;
-          }).filter(systemId => systemId);
-          if (systems.length === 2) {
-            strait.systems = systems as string[];
-          }
-          return strait as StraitDocumentInterface;
-        });
-      }
-      clusterDocData.systems = systemDocuments;
-      clusterDocData.straits = straitsWith_ids;
+    let straitsWith_ids: StraitDocumentInterface[] = [];
+    if (straits?.length) {
+      straitsWith_ids = straits.map((strait) => {
+        const systems = strait.systems.map((systemId) => {
+          return idsToObjectIds.get(systemId) as string | undefined;
+        }).filter(systemId => systemId);
+        if (systems.length === 2) {
+          strait.systems = systems as string[];
+        }
+        return strait as StraitDocumentInterface;
+      });
     }
-    const clusterDocument = new ClusterDocument(clusterDocData);
-    try {
-      await clusterDocument.save();
-    } catch (error) {
-      return [cluster?.id || 'UNKNOWN', error as Error];
-    }
-    return [cluster.id || 'UNKNOWN', clusterDocument];
+    clusterDocData.systems = systemDocuments;
+    clusterDocData.straits = straitsWith_ids;
   }
+  const clusterDocument = new ClusterDocument(clusterDocData);
+  try {
+    await clusterDocument.save();
+  } catch (error) {
+    return [cluster?.id || 'UNKNOWN', error as Error];
+  }
+  return [cluster.id || 'UNKNOWN', clusterDocument];
 }
 
 export default defineEventHandler(
@@ -86,9 +87,9 @@ export default defineEventHandler(
     console.log('clusterModelResults: ', clusterModelResults);
     const responseObj = {
       clusterModels
-    } as  {
-      clusterModels : ClusterModel[],
-      errors?: [string,Error][];
+    } as {
+      clusterModels: ClusterModel[],
+      errors?: [string, Error][];
     };
     if (errors.size) {
       responseObj.errors = [...errors];
