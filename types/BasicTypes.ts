615a9d0a4b24a63bc1d4f7b9f9305975577e9290
johnhelmuth <john@helmuth.org>
2025-04-08 06:01:49 -0700

    Removed all interfaces that included method specifications, hopefully simplifying the codebase. Removed all interfaces that included method specifications, hopefully simplifying the codebase.  Moved type definitions into the file that defines the model class for that type. Added type narrowing functions.

--- types/BasicTypes.ts
+++ types/BasicTypes.ts
@@ -15,16 +15,6 @@ export type attributeRatingMetaType = {
   color?: string
 };
 
-export type MapViewStylesType = 'data' | 'circular' | 'linear';
-export const mapViewStyleLabels = [
-  {key: 'data' as MapViewStylesType, label: 'Layout from data'},
-  {key: 'circular' as MapViewStylesType, label: 'Circular Layout'},
-  {key: 'linear' as MapViewStylesType, label: 'Linear Layout'},
-];
-export const MAP_VIEW_STYLES_DEFAULT = 'data' as MapViewStylesType;
-
-export type ClusterOrientationType = 'portrait' | 'square' | 'landscape';
-
 export type BoundingBoxType = {
   upperLeft: PointType;
   lowerRight: PointType;
