86673ee94090bc180d0dffc068b38cb793f7544e
johnhelmuth <john@helmuth.org>
2025-04-08 06:01:49 -0700

    Removed all interfaces that included method specifications, hopefully simplifying the codebase.  Moved type definitions into the file that defines the model class for that type. Added type narrowing functions.

--- stores/use-user-scope-store.ts
+++ stores/use-user-scope-store.ts
@@ -1,16 +1,14 @@
-import type {SelectedSystemsListInterface, SelectedSystemsServiceInterface} from "@/types/SystemsSelectedListTypes";
 
 import {SelectedSystemsService} from "@/models/SelectedSystemsService";
 import { RoutePlannerService } from "@/utils/route-planner-service";
-import type {RoutePlannerServiceInterface} from "@/types/RoutePlannerServiceTypes";
 
 const selectedSystemsService = new SelectedSystemsService();
 
 const routePlannerService = new RoutePlannerService();
 
 export function useUserScopeStore() : {
-  routePlannerService: RoutePlannerServiceInterface,
-  selectedSystemsService: SelectedSystemsServiceInterface,
+  routePlannerService: RoutePlannerService,
+  selectedSystemsService: SelectedSystemsService,
 } {
   return { routePlannerService, selectedSystemsService };
 }
