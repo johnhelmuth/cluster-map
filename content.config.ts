e19d534cd978612ed55f535cf29278499f647baa
John Helmuth <john@helmuth.org>
2025-04-27 21:46:18 -0700

    Added code to content config so I can easily test content changes locally.

--- content.config.ts
+++ content.config.ts
@@ -1,26 +1,33 @@
-import {defineContentConfig, defineCollection} from '@nuxt/content'
+import {defineContentConfig, defineCollection, type ResolvedCollectionSource} from '@nuxt/content'
 import {z} from "zod";
 
+const source = {
+  include: '**/*',
+  exclude: ['**/*.png', '**/*.svg', '**/*.jpg', 'LICENSE'],
+} as ResolvedCollectionSource;
+
+if (process.env.CONTENT_LOCAL_PATH) {
+  source.cwd = process.env.CONTENT_LOCAL_PATH;
+} else {
+  source.repository = process.env.GH_URL || 'https://github.com/johnhelmuth/in-dire-straits-content/tree/main';
+  source.authToken = process.env.GH_TOKEN;
+}
+
 export default defineContentConfig({
-    collections: {
-        content: defineCollection({
-            type: 'page',
-            source: {
-                include: '**/*',
-                exclude: ['**/*.png', '**/*.svg', '**/*.jpg', 'LICENSE'],
-                repository: 'https://github.com/johnhelmuth/in-dire-straits-content/tree/main',
-                authToken: process.env.GH_TOKEN,
-            },
-            schema: z.object({
-                'extra-nav-links': z.object({
-                    'pre': z.array(
-                        z.object({
-                            text: z.string(),
-                            'handler-tag': z.enum(['pick-random']),
-                        })
-                    )
-                })
+  collections: {
+    content: defineCollection({
+      type: 'page',
+      source,
+      schema: z.object({
+        'extra-nav-links': z.object({
+          'pre': z.array(
+            z.object({
+              text: z.string(),
+              'handler-tag': z.enum(['pick-random']),
             })
+          )
         })
-    }
+      })
+    })
+  }
 })
