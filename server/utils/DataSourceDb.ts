5572fcd372d58ef59acad2a1b1b3d47a717a8545
johnhelmuth <john@helmuth.org>
2025-04-30 08:52:26 -0700

    More work on database backend.
    Cleaned up API REST calls I don't currently need; sufficient unto the day.
    
    Fixed mis-matched universeID in database initialization code.

--- server/utils/DataSourceDb.ts
+++ server/utils/DataSourceDb.ts
@@ -1,18 +1,16 @@
-import {isUniverseMetadataData, UniverseMetadataData} from "~/models/UniversesManager";
+import {isUniverseMetadataData} from "~/models/UniversesManager";
 import {createSchemaValidationError, universeMetadataParse} from "~/utils/import-validator";
 import {mongo} from '#nuxt-mongodb';
 import * as mongoDB from 'mongodb';
 
-import {UniverseIdType, UniverseModelData} from "~/models/UniverseModel";
+import { UniverseIdType } from "~/models/UniverseModel";
 import {
   UniverseDataDocumentToUniverseMetadataData,
   UniverseDataDocumentToUniverseModelData,
   UniverseDataDocument, universeDataPipeline
 } from "~/server/document-models/UniverseDataDocument";
-import {SystemIdType} from "~/models/SystemModel";
-import {ClusterIdType} from "~/models/ClusterModel";
-import {ClusterDataDocument} from "~/server/document-models/ClusterDataDocument";
-import {SystemDataDocument} from "~/server/document-models/SystemDataDocument";
+import { ClusterDataDocument } from "~/server/document-models/ClusterDataDocument";
+import { SystemDataDocument } from "~/server/document-models/SystemDataDocument";
 
 export const collections: {
   universes?: mongoDB.Collection<UniverseDataDocument>,
@@ -69,26 +67,4 @@ export async function getUniverseData(universeId: UniverseIdType) {
   if (universesData) {
     return UniverseDataDocumentToUniverseModelData(universesData[0]);
   }
-}
-export async function getClustersData(clusterIds: Array<ClusterIdType>) {
-  const clustersData = await clustersCollection()
-    .find({
-      // TODO - clusterIds is an array of strings, needs to be an array of ObjectIds to match against _id?
-      // @ts-ignore
-      _id: {$in: clusterIds}
-    }).toArray();
-  return clustersData;
-}
-
-export async function getSystemsData(systemIds: Array<SystemIdType>) {
-  const systemsCursor = systemsCollection()
-    .find({
-      // TODO - systemIds is an array of strings, needs to be an array of ObjectIds to match against _id?
-      // @ts-ignore
-      _id: {$in: systemIds}
-    });
-  const systems = await systemsCursor.toArray();
-  console.log('systems: ', systems);
-  console.log('systems.length: ', systems.length);
-  return systems;
 }
