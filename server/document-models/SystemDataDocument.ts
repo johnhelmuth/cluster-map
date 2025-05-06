5e6ab25ccfa6ea08dc5c06ba49c3da48c094e3d0
johnhelmuth <john@helmuth.org>
2025-05-05 18:39:29 -0700

    Refactored to try to make the way I am handling the code dealing with the data coming out of the database more maintainable.
    Using a {Universe,Cluster,System,Strait}DataDocument class to handle the data coming out of the database, and to transform them into *ModelData to pass over the wire in the server API.
    
    Added missing `url` property to system database definitions.

--- server/document-models/SystemDataDocument.ts
+++ server/document-models/SystemDataDocument.ts
@@ -1,50 +1,142 @@
-import {Document, WithId} from "mongodb";
-import {SystemAttributesDefaults, SystemModelData} from "~/models/SystemModel";
+import {Document, ObjectId, WithId} from "mongodb";
+import {SystemAttributesInterface, SystemAttributesDefaults, SystemModelData} from "~/models/SystemModel";
 import {isPointType} from "~/types/GeometryTypes";
+import {SCHEMA_VERSION} from "~/constants";
 
-
-export interface SystemDataDocument extends WithId<Document> {
-  schemaVersion: string;
-  type: 'system';
-  name: string;
-  attributes: {
-    technology: number,
-    environment: number,
-    resources: number,
-  },
-  aspects: Array<string>,
+export class SystemDataDocument implements Omit<SystemModelData, "id">, WithId<Document> {
+  _id = new ObjectId();
+  schemaVersion: string = SCHEMA_VERSION;
+  type: 'system' = 'system';
+  name: string = 'Unknown system';
+  url?: string;
+  attributes: SystemAttributesInterface = {
+    technology: 0,
+    environment: 0,
+    resources: 0,
+  };
+  aspects: Array<string> = [];
   position: {
-    x: number,
-    y: number,
+    x : number,
+    y : number,
     z?: number
+  } = {
+    x: 500,
+    y: 500
   }
-}
 
-export function isSystemDataDocument(data: any): data is SystemDataDocument {
-  // TODO implement this.
-  return true;
-}
+  constructor(data: any) {
+    if (SystemDataDocument.isSystemDataDocument(data)) {
+      this._id = data._id;
+      this.schemaVersion = data.schemaVersion;
+      this.type = data.type;
+      this.name = data.name;
+      if (data?.url) {
+        this.url = data?.url;
+      }
+      this.attributes = data.attributes;
+      this.aspects = data.aspects;
+      this.position = data.position;
+    }
+  }
 
-/**
- * Converts mongo document with system data into plain system data.
- *
- * @param data {SystemDataDocument}
- *
- * @return {SystemModelData}
- */
-export function SystemDataDocumentToSystemModelData(data: SystemDataDocument) {
-  if (isSystemDataDocument(data)) {
+  toModelData() {
     const systemModelData = {
-      type: data.type,
-      schemaVersion: data.schemaVersion,
-      id: (data._id.toHexString()),
-      name: (data?.name || 'Unknown'),
-      attributes: data?.attributes || SystemAttributesDefaults,
-      aspects: data?.aspects || [],
+      type: this.type,
+      schemaVersion: this.schemaVersion,
+      id: (this._id.toHexString()),
+      name: (this?.name || 'Unknown'),
+      attributes: this?.attributes || SystemAttributesDefaults,
+      aspects: this?.aspects || [],
     } as SystemModelData;
-    if (isPointType(data.position)) {
-      systemModelData.position = data.position
+    if (typeof this?.url === 'string') {
+      systemModelData.url = this.url;
+    }
+    if (isPointType(this.position)) {
+      systemModelData.position = this.position
     }
     return systemModelData;
   }
+
+  static create(data: any): SystemDataDocument {
+    return new SystemDataDocument(data);
+  }
+
+  static isSystemDataDocument(data: any): data is SystemDataDocument {
+    // TODO implement this.
+    return true;
+  }
+
+  static mongoSchemaValidator() {
+    return {
+      "bsonType": "object",
+      "required": ["schemaVersion", "type", "name", "attributes", "aspects", "position"],
+      "properties": {
+        _id: {
+          "bsonType": "objectId"
+        },
+        "schemaVersion": {
+          "bsonType": "string"
+        },
+        "type": {
+          "bsonType": "string",
+          "enum": ["system"]
+        },
+        "name": {
+          "bsonType": "string"
+        },
+        "attributes": {
+          "bsonType": "object",
+          "required": ["technology", "environment", "resources"],
+          "properties": {
+            "technology": {
+              "description": "The rating for the Technology level of the system, range -4 to 4.",
+              "bsonType": "integer",
+              "minimum": -4,
+              "maximum": 4
+            },
+            "environment": {
+              "description": "The rating for the Environment level of the system, range -4 to 4.",
+              "bsonType": "integer",
+              "minimum": -4,
+              "maximum": 4
+            },
+            "resources": {
+              "description": "The rating for the Resources level of the system, range -4 to 4.",
+              "bsonType": "integer",
+              "minimum": -4,
+              "maximum": 4
+            }
+          }
+        },
+        "aspects": {
+          "bsonType": "array",
+          "items": {
+            "bsonType": "string",
+          }
+        },
+        "position": {
+          "bsonType": "object",
+          "required": ["x", "y"],
+          "properties": {
+            "x": {
+              "bsonType": "number",
+              "minimum": 0,
+              "maximum": 1000
+            },
+            "y": {
+              "bsonType": "number",
+              "minimum": 0,
+              "maximum": 1000
+            },
+            "z": {
+              "bsonType": "number",
+              "minimum": 0,
+              "maximum": 1000
+            }
+          }
+        }
+      }
+    };
+  }
+
 }
--- server/utils/DataSourceDb.ts
+++ server/utils/DataSourceDb.ts
@@ -1,70 +1,17 @@
-import {isUniverseMetadataData} from "~/models/UniversesManager";
+
 import {createSchemaValidationError, universeMetadataParse} from "~/utils/import-validator";
 import {mongo} from '#nuxt-mongodb';
 import * as mongoDB from 'mongodb';
 
-import { UniverseIdType } from "~/models/UniverseModel";
-import {
-  UniverseDataDocumentToUniverseMetadataData,
-  UniverseDataDocumentToUniverseModelData,
-  UniverseDataDocument, universeDataPipeline
-} from "~/server/document-models/UniverseDataDocument";
-import { ClusterDataDocument } from "~/server/document-models/ClusterDataDocument";
-import { SystemDataDocument } from "~/server/document-models/SystemDataDocument";
+import {UniverseDataDocument} from "~/server/document-models/UniverseDataDocument";
 
 export const collections: {
   universes?: mongoDB.Collection<UniverseDataDocument>,
-  clusters?: mongoDB.Collection<ClusterDataDocument>,
-  systems?: mongoDB.Collection<SystemDataDocument>,
 } = {};
 
-export function universesCollection() : mongoDB.Collection<UniverseDataDocument> {
-  if (! collections?.universes) {
+export function universesCollection(): mongoDB.Collection<UniverseDataDocument> {
+  if (!collections?.universes) {
     collections.universes = mongo.db().collection('universes');
-    // TODO Add schema validation here
   }
   return collections.universes;
-}
-
-export function clustersCollection() : mongoDB.Collection<ClusterDataDocument> {
-  if (! collections?.clusters) {
-    collections.clusters = mongo.db().collection('clusters');
-    // TODO Add schema validation here
-  }
-  return collections.clusters;
-}
-
-export function systemsCollection() : mongoDB.Collection<SystemDataDocument> {
-  if (! collections?.systems) {
-    collections.systems = mongo.db().collection('systems');
-
-  }
-  return collections.systems;
-}
-
-export async function getUniversesMetadataData() {
-  const universesMetadataCursor = universesCollection().find();
-
-  const rawUniversesMetadataData = [];
-
-  for await (const universeData of universesMetadataCursor) {
-    rawUniversesMetadataData.push(UniverseDataDocumentToUniverseMetadataData(universeData))
-  }
-  const universesMetadataData = rawUniversesMetadataData
-    .map((rawUniverseMetadataData) => {
-      if (!isUniverseMetadataData(rawUniverseMetadataData)) {
-        const parsedResponse = universeMetadataParse(rawUniverseMetadataData);
-        throw createSchemaValidationError(parsedResponse, 'DataSourceDb, invalid universeMetadataData. ');
-      }
-      return rawUniverseMetadataData;
-    });
-  return universesMetadataData;
-}
-
-export async function getUniverseData(universeId: UniverseIdType) {
-  const universesData = await universesCollection()
-    .aggregate(universeDataPipeline(universeId)).toArray() as UniverseDataDocument[];
-  if (universesData) {
-    return UniverseDataDocumentToUniverseModelData(universesData[0]);
-  }
 }
