c021e114475a5e57977b8e87d73413d353f216ae
johnhelmuth <john@helmuth.org>
2025-05-09 16:28:36 -0700

    Renamed userAuthDataDataDocument to userAuthDataDOcument; renamed all the schema and db initialization files to match (user-auth-data becomes user-auth, etc.)
    Added JSON-Schema schemas for user and user-auth models.
    
    Removed `salt` property from user-auth data, as bcrypt embeds that into the hash.
    
    Added test user/login post endpoint to figure out how to handle handling authentication of a username/password.

--- server/document-models/UserDataDocument.ts
+++ server/document-models/UserDataDocument.ts
@@ -1,20 +1,25 @@
 import {Document, ObjectId, WithId} from "mongodb";
 import {SCHEMA_VERSION} from "~/constants";
-import {type AuthenticationDataInterface, authenticationType, UserIdType, UserModelData} from "~/models/UserModel";
-import {usersCollection} from "~/server/utils/DataSourceDb";
+import {AuthenticationTypeType, UserIdType, UserModelData} from "~/models/UserModel";
+import {usersAuthCollection, usersCollection} from "~/server/utils/DataSourceDb";
+import {
+  UserAuthDataDocument,
+  UserAuthDataDocumentInterface
+} from "~/server/document-models/UserAuthDataDocument";
+import bcrypt from "bcryptjs";
 
 export interface UserDataDocumentInterface extends WithId<Document> {
   schemaVersion: string;
   type: 'user';
   name: string;
-  authenticationData: Array<AuthenticationDataInterface>;
+  authenticationData: Array<UserAuthDataDocumentInterface>;
 }
 
 export interface UserMetadataData {
   id: UserIdType;
   name: string;
   authenticationData: Array<{
-    authType: authenticationType;
+    authType: AuthenticationTypeType;
   }>
 }
 
@@ -23,7 +28,7 @@ export class UserDataDocument implements UserDataDocumentInterface {
   schemaVersion = SCHEMA_VERSION;
   type: 'user' = 'user';
   name: string = 'Unknown user';
-  authenticationData: Array<AuthenticationDataInterface> = [];
+  authenticationData: Array<UserAuthDataDocument> = [];
 
   constructor(data: any) {
     if (UserDataDocument.isUserDataDocument(data)) {
@@ -31,7 +36,18 @@ export class UserDataDocument implements UserDataDocumentInterface {
       this.schemaVersion = data.schemaVersion;
       this.type = data.type;
       this.name = data.name;
-      this.authenticationData = data.authenticationData;
+      if (data?.authenticationData
+        && Array.isArray(data?.authenticationData)
+        && data.authenticationData.every((authData) => {
+          return UserAuthDataDocument.isUserAuthDataDocument(authData);
+        })
+      ) {
+        const authenticationDataDocuments = data?.authenticationData
+          .map((authData) => {
+            return UserAuthDataDocument.create(authData);
+          })
+        this.authenticationData = authenticationDataDocuments;
+      }
     }
   }
 
@@ -44,36 +60,54 @@ export class UserDataDocument implements UserDataDocumentInterface {
     return new UserDataDocument(data);
   }
 
-  static async getUser(userId: UserIdType) {
-    const usersData = await (await usersCollection())
-      .aggregate(UserDataDocument.dataPipeline(userId)).toArray() as UserDataDocument[];
+  static async getUsersWhere(query: any, asModelData = true) {
+    const usersData = await usersCollection()
+      .aggregate(UserDataDocument.dataPipeline(query)).toArray() as UserDataDocument[];
     if (usersData) {
-      const userDataDocument = UserDataDocument.create(usersData[0]);
-      return userDataDocument.toModelData();
+      const usersModelData = usersData.map(
+        (userData) => {
+          const userDataDocument = UserDataDocument.create(userData);
+          if (userDataDocument) {
+            return asModelData ? userDataDocument.toModelData() : userDataDocument;
+          }
+        }
+      ).filter(umd => !! umd);
+      return usersModelData;
+    }
+  }
+
+  static async getUser(userId: UserIdType) {
+    const query = {
+      _id: {$eq: new ObjectId(userId)}
+    }
+    const userModelData = await UserDataDocument.getOneUserWhere(query);
+    if (userModelData) {
+      return userModelData;
+    }
+  }
+
+  static async getOneUserWhere(query: any) {
+    const usersModelData = await UserDataDocument.getUsersWhere(query);
+    if (usersModelData && Array.isArray(usersModelData) && usersModelData.length > 0) {
+      return usersModelData[0];
     }
   }
 
   static async getUsersMetadata() {
-    const usersData = await (await usersCollection())
-      .aggregate(UserDataDocument.dataPipeline()).toArray();
+    const usersData = await usersCollection()
+      .aggregate(UserDataDocument.dataPipeline({})).toArray();
     if (usersData) {
       const userDataDocuments = usersData.map(userData => {
         const userDataDocument = UserDataDocument.create(userData);
         const userMetadataData = userDataDocument.toUserMetadataData()
-        console.log('UserDataDocument.getUsersMetadata() userMetadataData: ', userMetadataData);
         return userMetadataData;
       })
-      console.log('UserDataDocument.getUsersMetadata() userDataDocuments: ', userDataDocuments);
       return userDataDocuments;
     }
   }
 
-  static dataPipeline(userId?: UserIdType) {
-    const matchQuery = userId
-      ? {
-        _id: {$eq: new ObjectId(userId)}
-      }
-      : {};
+  static dataPipeline(query: any) {
+    const matchQuery = query ? query : {};
     return [
       {
         $match: matchQuery
@@ -105,18 +139,52 @@ export class UserDataDocument implements UserDataDocumentInterface {
       schemaVersion: this.schemaVersion,
       type: this.type,
       name: this.name,
-      authenticationData: this.authenticationData
+      authenticationData: this.authenticationData.map(authData => {
+        return authData.toModelData()
+      })
     } as UserModelData;
   }
 
   toUserMetadataData(): UserMetadataData {
-    console.log('this.authenticationData', this.authenticationData);
     return {
       id: this._id.toHexString(),
       name: this.name,
       authenticationData: this.authenticationData.map(authData => {
-        return { authType: authData.authType };
+        return {authType: authData.authType};
       })
     }
   }
+
+  static async login(data: LoginDocument | any) {
+    if (isLoginDocument(data)) {
+      const userAuth = await usersAuthCollection()
+        .findOne({
+          authType: 'username-password',
+          'usernamePassword.username': data.username
+        });
+      console.log('UserDataDocument.login() userAuth: ', userAuth);
+      if (userAuth?.usernamePassword) {
+        const matches = await bcrypt.compare(data.password, userAuth.usernamePassword.hash);
+        console.log('UserDataDocument.login() matches: ', matches);
+        if (matches) {
+          const userDataDocument = await UserDataDocument.getOneUserWhere({
+            authenticationData: userAuth._id
+          });
+          console.log('UserDataDocument.login() userDataDocument: ', userDataDocument);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
+
+export type LoginDocument = { username: string, password: string };
+
+function isLoginDocument(data: any): data is LoginDocument {
+  return (
+    data &&
+    data?.username && typeof data?.username === 'string' &&
+    data?.password && typeof data?.password === 'string'
+  );
 }
