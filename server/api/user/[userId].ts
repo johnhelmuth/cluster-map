1228ef7d65d9a29049a07f274d64f87e0c263658
johnhelmuth <john@helmuth.org>
2025-06-26 15:02:07 -0700

    Added users list page.

--- server/api/user/[userId].ts
+++ server/api/user/[userId].ts
@@ -1,9 +1,12 @@
 import {UserDataDocument} from "~/server/document-models/UserDataDocument";
 
 export default defineEventHandler(async (event ) => {
+  await requireUserSession(event);
+
   const userId = getRouterParam(event, 'userId');
+
   if (userId) {
     const userData = await UserDataDocument.getUser(userId);
-    return userData;
+    return userData?.toUserMetadataData();
   }
 })
