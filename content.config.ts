d0cf8eb76c558e6fdbb51ce1e1fdbc418287e560
John Helmuth <john@helmuth.org>
2025-09-26 08:45:08 -0700

    Define character sheets as content, minor code tweaks. Moved sample sheets to in-dire-strait-content repo.

--- content.config.ts
+++ content.config.ts
@@ -1,23 +1,33 @@
 import {type ResolvedCollectionSource, defineContentConfig, defineCollection} from '@nuxt/content'
 import {z} from "zod";
+import { convertJsonSchemaToZod } from 'zod-from-json-schema';
+import characterSchema from './data/schemas/characters/character.schema.json';
 
-const source = {
+const zodCharacterSchema = convertJsonSchemaToZod(characterSchema);
+const contentSource = {
   include: '**/*',
-  exclude: ['**/*.png', '**/*.svg', '**/*.jpg', '**/*.jpeg', 'LICENSE', '.git', '**/.DS_Store', '**/.idea'],
+  exclude: ['characters/**/*.json', '**/*.png', '**/*.svg', '**/*.jpg', '**/*.jpeg', 'LICENSE', '.git', '**/.DS_Store', '**/.idea'],
+} as ResolvedCollectionSource;
+
+const characterSource = {
+  include: 'characters/**/*.json',
 } as ResolvedCollectionSource;
 
 if (process.env.CONTENT_LOCAL_PATH) {
-  source.cwd = process.env.CONTENT_LOCAL_PATH;
+  contentSource.cwd = process.env.CONTENT_LOCAL_PATH;
+  characterSource.cwd = process.env.CONTENT_LOCAL_PATH;
 } else {
-  source.repository = process.env.GH_URL || 'https://github.com/johnhelmuth/in-dire-straits-content/tree/main';
-  source.authToken = process.env.GH_TOKEN;
+  contentSource.repository = process.env.GH_URL || 'https://github.com/johnhelmuth/in-dire-straits-content/tree/main';
+  contentSource.authToken = process.env.GH_TOKEN;
+  characterSource.repository = process.env.GH_URL || 'https://github.com/johnhelmuth/in-dire-straits-content/tree/main';
+  characterSource.authToken = process.env.GH_TOKEN;
 }
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: 'page',
-      source,
+      source: contentSource,
       schema: z.object({
         'extra-nav-links': z.object({
           'pre': z.array(
@@ -30,6 +40,11 @@ export default defineContentConfig({
         'in_game_start': z.string(),
         'in_game_end': z.string(),
       })
+    }),
+    characters: defineCollection({
+      type: 'data',
+      source: characterSource,
+      schema: zodCharacterSchema
     })
   }
 })
